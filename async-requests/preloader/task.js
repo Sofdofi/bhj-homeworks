2d12f91490f3d16b80c81f7d0e76d54fd957644f
Sofdofi <sanyachka.kush@yandex.ru>
2023-09-20 13:42:02 +0300

    first commit preloader

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,46 +1,18 @@
-const poolAnswers = document.getElementById('poll__answers');
+'use strict'
+const items = document.getElementById('items')
+const loader = document.querySelector('.loader')
+const url = 'https://students.netoservices.ru/nestjs-backend/slow-get-courses'
 
-// Создаем запрос на получение данных
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
-xhr.addEventListener('load', function() {
-    if(xhr.readyState === xhr.DONE) {
-        // Если запрос завершился ошибкой 400+
-        if(xhr.status >= 400) {
-            setTimeout(() => {
-                return alert('Ошибка загрузки данных о вопросе');
-            }, 1000)
+fetch(url)
+    .then((data) => data.text())
+    .then(function (data) {
+        loader.classList.remove('loader_active')
+        let valute = JSON.parse(data).response.Valute
+        for (let key in valute) {
+            items.innerHTML += ` <div class="item">
+                 <div class="item__code">${valute[key].CharCode}</div>
+                 <div class="item__value">${valute[key].Value}</div>
+                 <div class="item__currency">руб.</div>
+                 <div></div>`
         }
-        // Если запрос завершился успешно
-        if(xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            const poolTitleText = response.data.title;
-            const poolAnswersText = response.data.answers;
-
-            const poolTitle = document.getElementById('poll__title');
-            poolTitle.textContent = poolTitleText;
-
-            poolAnswersText.forEach(answer => {
-                createHTML(answer);
-            });
-
-            const btnAnswers = document.querySelectorAll('.poll__answer');
-            btnAnswers.forEach(btn => {
-                btn.addEventListener('click', () => {
-                    alert('Спасибо, Ваш голос засчитан!');
-                    
-                })
-            })
-        }
-    }
-})
-xhr.send();
-
-// Функция создания кнопки
-function createHTML(answer) { 
-    const btnAnswer = document.createElement('button');
-    btnAnswer.classList.add('poll__answer');
-    btnAnswer.textContent = answer;
-    poolAnswers.appendChild(btnAnswer);
-    return btnAnswer;
-}
+    })
